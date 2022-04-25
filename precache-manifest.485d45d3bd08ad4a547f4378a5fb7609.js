5c210a331254226c38fe428f2f610b493908b914
Ryan Dowd <ryandowd@Ryans-MacBook-Pro.local>
2022-04-25 14:30:02 +1000

    Adding latest Sky employment

---
+++ precache-manifest.485d45d3bd08ad4a547f4378a5fb7609.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd50f250bfe3da75df91",
+    "url": "/static/css/main.99c0a46f.chunk.css"
+  },
+  {
+    "revision": "fd50f250bfe3da75df91",
+    "url": "/static/js/main.fd50f250.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1ab61a15eab6ed753bf6",
+    "url": "/static/js/2.1ab61a15.chunk.js"
+  },
+  {
+    "revision": "01e586416c5103d59176777155daee43",
+    "url": "/static/media/logo-haymarket.01e58641.png"
+  },
+  {
+    "revision": "ea3e24309d4a633352a1954fa0fb29f7",
+    "url": "/static/media/logo-sky.ea3e2430.png"
+  },
+  {
+    "revision": "c23f497345bd0fe5edefd793914cdb62",
+    "url": "/static/media/tech-aws.c23f4973.png"
+  },
+  {
+    "revision": "f1c6f36aa0bfb8f58c60077a5822b740",
+    "url": "/static/media/tech-node.f1c6f36a.png"
+  },
+  {
+    "revision": "a10918066db9f6204ba03672674be306",
+    "url": "/static/media/tech-webpack.a1091806.png"
+  },
+  {
+    "revision": "42644325337a073d4907e83945f47f57",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49b1908f522f7b9b28a1616660ba0c97.js"
+  "/precache-manifest.485d45d3bd08ad4a547f4378a5fb7609.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
